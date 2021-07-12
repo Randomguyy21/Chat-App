bb5d1ac17af01ea107aba2e0a53368145a5c583a
Randomguyy21 <67327168+Randomguyy21@users.noreply.github.com>
2021-07-12 16:18:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
+
+const App= () => {
+    return (
+        <ChatEngine
+        height= "100vh"
+        projectID="16123761-9bea-4979-abaa-ecf1318ecbf0"
+        userName="Randomguy"
+        userSecret="abcde"
+        />
+    )
+}
+export default App;
